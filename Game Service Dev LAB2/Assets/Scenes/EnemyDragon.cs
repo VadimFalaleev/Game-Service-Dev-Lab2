3c0febc9191e41e6755bb72876e2367b0d9c4c2d
Вадим <54228342+VadimFalaleev@users.noreply.github.com>
2022-09-30 17:33:36 +0500

    Video2

---
+++ Game Service Dev LAB2/Assets/Scenes/EnemyDragon.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class EnemyDragon : MonoBehaviour
+{
+    public GameObject dragonEggPrefab;
+    public float speed = 1f;
+    public float timeBetweenEggDrops = 1f;
+    public float leftRightDistance = 10f;
+    public float chanceDirection = 0.1f;
+
+    private void Update()
+    {
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
+
+        if (pos.x < -leftRightDistance)
+            speed = Mathf.Abs(speed);
+        else if (pos.x > leftRightDistance)
+            speed = -Mathf.Abs(speed);
+    }
+
+    private void FixedUpdate()
+    {
+        if (Random.value < chanceDirection)
+            speed *= -1;
+    }
+}
